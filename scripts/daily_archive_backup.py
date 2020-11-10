f5bd0c50e1d5dec4b4ec7d521d3059e20297b2bb
akrherz <akrherz@iastate.edu>
2020-11-10 09:38:32 -0600

    improve code and error on small file size

--- scripts/daily_archive_backup.py
+++ scripts/daily_archive_backup.py
@@ -5,9 +5,12 @@ Lets run at 12z for the previous date
 import datetime
 import subprocess
 import os
-import logging
+import sys
+from pathlib import Path
 
-logger = logging.getLogger()
+from pyiem.util import logger
+
+LOG = logger()
 
 
 def run(date):
@@ -15,22 +18,31 @@ def run(date):
     os.chdir("/data/tmp")
     tarfn = date.strftime("mtarchive_%Y%m%d.tgz")
     # Step 1: Create a gzipped tar file
-    cmd = "tar -czf %s /mnt/mtarchive/data/%s" % (tarfn,
-                                                  date.strftime("%Y/%m/%d"))
+    cmd = "tar -czf %s /mnt/mtarchive/data/%s" % (tarfn, date.strftime("%Y/%m/%d"))
+    LOG.debug(cmd)
     subprocess.call(cmd, shell=True, stderr=subprocess.PIPE)
+    sz = Path(tarfn).stat().st_size
+    LOG.debug("%s has size %s", tarfn, sz)
+    if sz < 1000:
+        LOG.info("%s is too small! %s bytes, aborting", tarfn, sz)
+        os.unlink(tarfn)
+        return
     remotedir = date.strftime("/stage/DailyMetArchive/%Y/%m")
     cmd = (
-        "rsync -a --remove-source-files --rsync-path \"mkdir -p %s; rsync\" "
+        'rsync -a --remove-source-files --rsync-path "mkdir -p %s; rsync" '
         "%s meteor_ldm@metl60.agron.iastate.edu:%s"
     ) % (remotedir, tarfn, remotedir)
     subprocess.call(cmd, shell=True)
 
 
-def main():
+def main(argv):
     """Go Main Go!"""
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    run(yesterday)
+    if len(argv) == 4:
+        date = datetime.date(int(argv[1]), int(argv[2]), int(argv[3]))
+    else:
+        date = datetime.date.today() - datetime.timedelta(days=1)
+    run(date)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(sys.argv)
