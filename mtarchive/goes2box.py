05876318445e399b3796ac89fbd8cd104f019c38
akrherz <akrherz@iastate.edu>
2020-12-11 14:25:44 -0600

    correct link to Google Drive

--- mtarchive/goes2box.py
+++ mtarchive/goes2box.py
@@ -10,64 +10,72 @@ from pyiem.util import logger
 
 LOG = logger()
 TMPDIR = "/isu/mtarchive/tmp"
+ARCHIVE = "https://drive.google.com/drive/folders/1CaKEmYK5UBio7iro_6JjiLOFJiPvBLaj"
 
 
 def run(bird, dt, offset, sector):
     """Run for a given date."""
     dt = dt - datetime.timedelta(days=offset)
-    path = (
-        "/isu/mtarchive/data/%s/cod/sat/goes%s/%s"
-    ) % (dt.strftime("%Y/%m/%d"), bird, sector)
+    path = ("/isu/mtarchive/data/%s/cod/sat/goes%s/%s") % (
+        dt.strftime("%Y/%m/%d"),
+        bird,
+        sector,
+    )
     if not os.path.isdir(path):
         if offset == 0:
-            print("goes2box %s not found" % (path, ))
+            print("goes2box %s not found" % (path,))
         return
     os.chdir(path)
     zips = []
     for dirname in os.listdir("."):
-        if dirname == 'HEADER.html':
+        if dirname == "HEADER.html":
             continue
-        zipfn = "goes%s_%s_%s_%s.zip" % (
-            bird, sector, dirname, dt.strftime("%Y%m%d"))
+        zipfn = "goes%s_%s_%s_%s.zip" % (bird, sector, dirname, dt.strftime("%Y%m%d"))
         LOG.debug("creating %s", zipfn)
         proc = subprocess.Popen(
             "zip -q -r %s/%s %s" % (TMPDIR, zipfn, dirname),
-            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
         )
         (stdout, stderr) = proc.communicate()
-        if (stdout.decode('ascii', 'ignore') == '' and
-                stderr.decode('ascii', 'ignore') == ''):
-            subprocess.call("rm -rf %s" % (dirname, ), shell=True)
+        if (
+            stdout.decode("ascii", "ignore") == ""
+            and stderr.decode("ascii", "ignore") == ""
+        ):
+            subprocess.call("rm -rf %s" % (dirname,), shell=True)
         else:
-            print("goes2box processing goes%s/%s resulted in %s %s" % (
-                bird, dirname, stdout, stderr))
+            print(
+                "goes2box processing goes%s/%s resulted in %s %s"
+                % (bird, dirname, stdout, stderr)
+            )
         zips.append(zipfn)
-    with open("HEADER.html", 'w') as fh:
-        fh.write((
-            "%s imagery found <a href=\""
-            "https://iastate.box.com/s/biv6ungdrto2pyjufyg5sz9j98jlwg9u\""
-            ">on CyBox</a>") % (sector, ))
+    with open("HEADER.html", "w") as fh:
+        fh.write(f'{sector} imagery found <a href="{ARCHIVE}">on Google Drive</a>')
     if not zips:
         return
     os.chdir(TMPDIR)
     dirname = "/stage/mtarchive/%s/%02i/%02i/cod/sat/goes%s/%s" % (
-        dt.year, dt.month, dt.day, bird, sector)
-    rsyncpath = "mkdir -p %s && rsync" % (dirname, )
-    cmd = (
-        "rsync -a --rsync-path=\"%s\" --remove-source-files "
-        "%s meteor_ldm@metl60.agron.iastate.edu:%s/"
-        ) % (
-            rsyncpath,
-            " ".join(zips), dirname
+        dt.year,
+        dt.month,
+        dt.day,
+        bird,
+        sector,
     )
+    rsyncpath = "mkdir -p %s && rsync" % (dirname,)
+    cmd = (
+        'rsync -a --rsync-path="%s" --remove-source-files '
+        "%s meteor_ldm@metl60.agron.iastate.edu:%s/"
+    ) % (rsyncpath, " ".join(zips), dirname)
     proc = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     stdout = proc.stdout.read()
     stderr = proc.stderr.read()
-    if stdout != b'' or stderr != b'':
+    if stdout != b"" or stderr != b"":
         print(cmd)
-        print(stdout.decode('ascii', 'ignore'))
-        print(stderr.decode('ascii', 'ignore'))
+        print(stdout.decode("ascii", "ignore"))
+        print(stderr.decode("ascii", "ignore"))
 
 
 def main(argv):
@@ -80,9 +88,9 @@ def main(argv):
     for offset in (0, 1, 14, 250, 800):
         LOG.debug("processing offset %s", offset)
         for bird in (16, 17):
-            for sector in ['global', 'meso', 'subregional']:
+            for sector in ["global", "meso", "subregional"]:
                 run(bird, dt, offset, sector)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv)
